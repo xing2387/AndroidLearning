f1674ebe56889c8983e48caaad94828968f7bf62
xing2387 <xing2387@gmail.com>
2017-10-30 16:14:54 +0800

    修改目录结构，增加 ruleview

--- app/src/main/java/com/example/xing/androidlearning/util/CommomUtil.java
+++ app/src/main/java/com/example/xing/androidlearning/util/CommomUtil.java
@@ -9,13 +9,15 @@ import android.util.TypedValue;
 public class CommomUtil {
 
     /**
-     *  将unit对应的单位的大小转为像素
-     * @param unit  TypeValue.COMPLEX_UNIT_*
-     * @param size  size
-     * @return  size in px
+     * 将unit对应的单位的大小转为像素
+     *
+     * @param unit TypeValue.COMPLEX_UNIT_*
+     * @param size size
+     * @return size in px
      */
     public static float getRawSize(int unit, float size) {
         Resources r = Resources.getSystem();
         return TypedValue.applyDimension(unit, size, r.getDisplayMetrics());
     }
+
 }
